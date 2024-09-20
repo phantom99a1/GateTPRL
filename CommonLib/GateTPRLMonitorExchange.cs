85820605d5fe76f5b30fefdd25c3225570b929bc
DuatPT <duatpt2@navisoft.com.vn>
2024-09-20 10:51:15 +0700

    Fix #ITmonitor: thêm comment ghi chú khi tạo class

--- CommonLib/GateTPRLMonitorExchange.cs
+++ CommonLib/GateTPRLMonitorExchange.cs
@@ -1,5 +1,6 @@
 ﻿//#2024.09.20 Revie code => thêm comment rõ mục đich
 //Comment rõ mục đích để làm gì 
+//Dùng để hiển thị các dữ liệu về số lượng message Exchange đã gửi, Exchange đã nhận và đang trong hàng đợi chưa gửi tới Exchange
 namespace CommonLib
 {
     public class GateTPRLMonitorExchange
